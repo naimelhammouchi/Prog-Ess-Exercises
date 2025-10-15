217161bbc6f78427cbe4a6fe446a8c8cfe153f3c
Naïm El Hammouchi <naimelhammouchi@icloud.com>
2025-10-15 15:28:31 +0200

    opdracht array en methode gedaan

--- opdrachten/oefVariabeleArayenStringMethode.mjs
+++ opdrachten/oefVariabeleArayenStringMethode.mjs
@@ -1,29 +1,35 @@
 // 1) oefening op variable: Geef  naam en  lievelingsgetal  of je leeftijd ( mg je zelf kiezen )  in een 2 variabelen  sla op en druk af.
 
-/*import * as readline from 'node:readline/promises';
+Antwoord: 
+import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 let naam = await userInput.question('Wat is je naam? ');
 let leeftijd = await userInput.question('Wat is je leeftijd? ');
 console.log('Je naam is ' + naam + ' en je leeftijd is ' + leeftijd);
 
-process.exit(); */
+process.exit();
 
-/* 2) oefening op datatype en Array
 
-Bespreek  in 5 regels en eigen bewoording de verschillende datatypes: strings, getallen, booleans, arrays, en objecten.
 
-Oefening:  maak een array met je  favoriete fruit(en)  en druk af */
 
-/* Antwoorden: 
-strings -> is een dataype die vooral characters of stukken teksten bevat;
-getallen -> is een datatype die numerieke waarden bevatten, gehele getallen of zelfs komagetallen;
-booleans -> is een datatype die 2 mogelijke waarden kan aangeven namelijk true of false; en die gebruikt worden voor logische operaties;
-arrays -> is een datatype die meerdere waarden kan opslaan in een enkele variabele, en die geordend zijn;
-objecten -> is een datatype die complexe datastructuren kan opslaan met key/value waarden.
-*/
+// 2) oefening op datatype en Array
+// a) Bespreek  in 5 regels en eigen bewoording de verschillende datatypes: strings, getallen, booleans, arrays, en objecten.
 
-/* let fruit = [
+Antwoorden: 
+strings is een datatype die vooral characters of stukken teksten bevat.
+getallen is een datatype die gehele getallen of  komagetallen bevat.
+booleans is een datatype die 2 mogelijke waarden kan aangeven namelijk true of false, en die gebruikt worden voor logische operaties.
+arrays is een datatype die meerdere waarden kan opslaan in een enkele variabele, en die geordend zijn.
+objecten is een datatype die complexe datastructuren kan opslaan met key/value waarden.
+
+
+
+// b) maak een array met je  favoriete fruit(en)  en druk af
+
+
+
+let fruit = [
   'kersen',
   'mango',
   'passievrucht',
@@ -32,9 +38,13 @@ objecten -> is een datatype die complexe datastructuren kan opslaan met key/valu
   'meloen',
   'druiven',
 ];
-console.log('mijn favoriete fruiten zijn: ' + fruit); */
+console.log('mijn favoriete fruiten zijn: ' + fruit); 
+
+
 
 // oefening 3 Gebruik 3 string-methodes op 1 variabele
+
+Antwoorden: 
 let string = 'Hey mijn naam is naïm en ik ben 26 jaar oud';
 let upper = string.toUpperCase();
 console.log(string);
