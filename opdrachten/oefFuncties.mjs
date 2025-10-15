62a959c4ac5228f696c2213ad529cd255d2a5ef0
Naïm El Hammouchi <naimelhammouchi@icloud.com>
2025-10-15 15:39:12 +0200

    oefFuncties gedaan

--- opdrachten/oefFuncties.mjs
+++ opdrachten/oefFuncties.mjs
@@ -1,29 +1,36 @@
 //oefening 1: Leg uit wat functies zijn en hoe je ze definieert en aanroept.  (  eigen bewoording is prima  Doe dit beknopt tussen 2  en max 5 regels tekst  )
 
-/* Antwoord:  
+Antwoord:  
+Functies zijn herbruikbare blokken code die een specifieke taak uitvoeren.
+Je definieert een functie met het sleutelwoord 'function', gevolgd door de functienaam en haakjes met eventueel één of meer parameters.
+Als je een functie wilt gebruiken, roep je de functienaam aan met haakjes en geef je de eventuele argumenten door.
 
-funcies zijn herbruikbare blokken code die een specifieke taak uitvoeren.Je defineert een functie met als eerst woord 'function', gevolgd door de functienaam en haakjes met eventueel een parameter of paramerters.;
-Als je de functies wilt gebruiken, roep je de functienaam aan met haakjes en geef je de eventuele arguementen door.
 
-*/
+//oefening 2: 
+// a) Maak een functie ( naam optellen ) die twee getallen optelt en het resultaat teruggeeft.
 
-//oefening 2: Maak  een functie   ( naam optellen ) die twee getallen optelt en het resultaat teruggeeft. Als dit werkt maak je nog een functie verschil , vermenigvuldiging en deling
-
-function som(a, b) {
+Antwoord:
+function optellen(a, b) {
   return a + b;
 }
-console.log(som(10, 5));
+console.log(optellen(10, 5));
 
+//b) Maak een functie verschil
+Antwoord:
 function verschil(a, b) {
   return a - b;
 }
 console.log(verschil(10, 5));
 
+// c) Maak een functie vermenigvuldiging
+Antwoord:
 function vermenigvuldiging(a, b) {
   return a * b;
 }
 console.log(vermenigvuldiging(10, 5));
 
+// d) Maak een functie deling
+Antwoord:
 function deling(a, b) {
   return a / b;
 }
